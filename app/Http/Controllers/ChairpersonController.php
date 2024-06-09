f7efd3309f7b93684fde01763608a2975299b9fc
Fred Taran <ftaran04@gmail.com>
2024-06-09 16:31:07 +0800

    Fixes 6/9/2024

--- app/Http/Controllers/ChairpersonController.php
+++ app/Http/Controllers/ChairpersonController.php
@@ -27,15 +27,25 @@ class ChairpersonController extends Controller
 
     // Get all the students subjected for accreditation
     public function get_students() {
+        // $students = Student::whereHas('credited_subject', function($query) {
+        //                         $query->whereHas('subject', function($x) {
+        //                             $x->whereHas('course', function($y) {
+        //                                 $y->whereHas('chairperson', function($z) {
+        //                                     $z->where('id', auth()->user()->id);
+        //                                 });
+        //                             });
+        //                         })->where('status', 1);
+        //                     })
+        //                     ->with('course')->get();
+
         $students = Student::whereHas('credited_subject', function($query) {
-                                $query->whereHas('subject', function($x) {
-                                    $x->whereHas('course', function($y) {
-                                        $y->whereHas('chairperson', function($z) {
-                                            $z->where('id', auth()->user()->id);
-                                        });
+                                $query->whereHas('subject', function($query2) {
+                                    $query2->whereHas('approver_program', function($query3) {
+                                        $query3->where('chairperson_id', auth()->user()->id);
                                     });
                                 })->where('status', 1);
                             })
+                            ->with('credited_subject.subject.approver_program')
                             ->with('course')->get();
 
         return response()->json([
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,38 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         $data = [
+            // [
+            //     'first_name' => 'Fred',
+            //     'last_name' => 'Taran',
+            //     'email' => 'ftaran100411@gmail.com',
+            //     'username' => 'fred.taran',
+            //     'role' => 2,
+            //     'password' => bcrypt('@dmin123'),
+            // ],
+            // [
+            //     'first_name' => 'April Rose',
+            //     'last_name' => 'Legaspi',
+            //     'email' => 'a@mail.com',
+            //     'username' => 'aprilrose.legaspi',
+            //     'role' => 2,
+            //     'password' => bcrypt('@dmin123'),
+            // ],
+            // [
+            //     'first_name' => 'Dean 1',
+            //     'last_name' => 'Dean 1',
+            //     'email' => 'b@mail.com',
+            //     'username' => 'dean1.dean1',
+            //     'role' => 3,
+            //     'password' => bcrypt('@dmin123'),
+            // ],
+            // [
+            //     'first_name' => 'Dean 2',
+            //     'last_name' => 'Dean 2',
+            //     'email' => 'c@mail.com',
+            //     'username' => 'dean2.dean2',
+            //     'role' => 3,
+            //     'password' => bcrypt('@dmin123'),
+            // ],
             [
                 'first_name' => 'Admin',
                 'last_name' => 'Admin',
